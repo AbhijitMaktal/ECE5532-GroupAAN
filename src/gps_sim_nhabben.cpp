44d492a8782c2cf4d07368372e6a01bd707cf15b
nick <nhabben@oakland.edu>
2021-04-14 20:35:37 -0400

    Right Turn

--- src/gps_sim_nhabben.cpp
+++ src/gps_sim_nhabben.cpp
@@ -52,30 +52,52 @@ std_msgs::Float64 cmd_brake;
 
 double atPoint = 0;
 
+//list of points to travel
+std::vector<double> waypoint_path;
+
+
 void timerCallback(const ros::TimerEvent& event){
    //should only init
 
-  waypoint_lat.resize(8);
-  waypoint_lat[0] = 41.9997801092; // B
-  waypoint_lat[1] = 42.0003648088; //C
-  waypoint_lat[2] = 42.852443;
-  waypoint_lat[3] = 42.852021;
-  waypoint_lat[4] = 42.851525;
-  waypoint_lat[5] = 42.851344;
-  waypoint_lat[6] = 42.850836;
-  waypoint_lat[7] = 42.849644;
-  waypoint_lon.resize(8);
-  waypoint_lon[0] = -82.9956864214; // B
-  waypoint_lon[1] = -82.995706772; // C
-  waypoint_lon[2] = -83.068013;
-  waypoint_lon[3] = -83.066888;
-  waypoint_lon[4] = -83.067044;
-  waypoint_lon[5] = -83.066344;
-  waypoint_lon[6] = -83.066440;
-  waypoint_lon[7] = -83.066060;
+  waypoint_lat.resize(17);
+  waypoint_lat[0] = 42.00244378; 
+  waypoint_lat[1] = 41.99978011; 
+  waypoint_lat[2] = 42.00036481;
+  waypoint_lat[3] = 42.00033951;
+  waypoint_lat[4] = 42.00032474;
+  waypoint_lat[5] = 42.0013603;
+  waypoint_lat[6] = 42.00266004;
+  waypoint_lat[7] = 42.00138482;
+  waypoint_lat[8] = 42.00139909; 
+  waypoint_lat[9] = 42.00323055; 
+  waypoint_lat[10] = 42.00272746;
+  waypoint_lat[11] = 42.00264799;
+  waypoint_lat[12] = 42.00092496;
+  waypoint_lat[13] = 42.00256814;
+  waypoint_lat[14] = 42.00162285;
+  waypoint_lat[15] = 42.00113908;
+  waypoint_lat[16] = 42.00242659;
+  waypoint_lon.resize(17);
+  waypoint_lon[0] = -83.0018947; 
+  waypoint_lon[1] = -82.99568642; 
+  waypoint_lon[2] = -82.99570677;
+  waypoint_lon[3] = -82.99709325;
+  waypoint_lon[4] = -82.99787731;
+  waypoint_lon[5] = -82.99791196;
+  waypoint_lon[6] = -83.0005567;
+  waypoint_lon[7] = -82.99652292;
+  waypoint_lon[8] = -82.99573927; 
+  waypoint_lon[9] = -82.99770135; 
+  waypoint_lon[10] = -82.99698865;
+  waypoint_lon[11] = -82.99768045;
+  waypoint_lon[12] = -82.99711256;
+  waypoint_lon[13] = -82.99448124;
+  waypoint_lon[14] = -82.9931558;
+  waypoint_lon[15] = -82.99513235;
+  waypoint_lon[16] = -83.00267861;
 
-  relative_position_waypoint.resize(8);
-  for (int k = 0; k < 8; k++) {
+  relative_position_waypoint.resize(17);
+  for (int k = 0; k < 17; k++) {
     UTMCoords waypoint(LatLon(waypoint_lat[k], waypoint_lon[k], 0.0));
     relative_position_waypoint[k] = waypoint - ref_coords;
   }
@@ -84,7 +106,8 @@ void timerCallback(const ros::TimerEvent& event){
   current_pose.pose.position.x = relative_position.x();
   current_pose.pose.position.y = relative_position.y();
   //Use i to shorten code
-  int i = current_waypoint; 
+  // int i = current_waypoint; 
+  int i = waypoint_path[current_waypoint];
   
   //Needed for Term Project????
   /*
@@ -126,15 +149,42 @@ void timerCallback(const ros::TimerEvent& event){
 
   // pub_vel.publish(cmd_vel);
   // pub_steering.publish(cmd_steering);
-    
+
+  double turnDir;
+  double turnDist;
+  if (current_waypoint== 2)
+  {
+    turnDir = 2; //Right
+    turnDist = 8;
+  }
+  else{
+    turnDir = 1; //Left
+    turnDist = 8;
+  }
+
+
   cmd_throttle.data = 0.1;
   cmd_brake.data = 0;
   
-  if (dist < 8){
+  if (dist < turnDist){
     // cmd_throttle.data = 0;
     // cmd_brake.data = 100;
-     cmd_steering.data = 4.8; // 10 8 4
-     atPoint = 1;
+
+    if (current_waypoint == 3){
+      cmd_throttle.data = 0;
+      cmd_brake.data = 1000;
+    }
+    else
+    {
+      atPoint = 1;
+      if (turnDir ==1) {
+        cmd_steering.data = 4.8; // 10 8 4
+      }
+      else if (turnDir == 2)
+      {
+        cmd_steering.data = -9;
+      }
+    }
   }
   else{
     if (atPoint == 1) 
@@ -160,7 +210,9 @@ void timerCallback(const ros::TimerEvent& event){
   ROS_INFO("Waypoint UTM: (%f, %f)", waypoint_coords.getX(), waypoint_coords.getY());
   ROS_INFO("Waypoint Relative Position: (%f, %f)", relative_position_waypoint[i].x(), relative_position_waypoint[i].y());
   ROS_INFO("Calculated Distance: (%f)",  dist);
-
+  ROS_INFO("i: (%d)",  i);
+  ROS_INFO("current_waypoint: (%d)",  current_waypoint);
+  
 }
 
 void recvFix(const sensor_msgs::NavSatFixConstPtr& msg){
@@ -194,7 +246,14 @@ int main(int argc, char** argv){
 
   double ref_lat;
   double ref_lon;
-  current_waypoint = 0;
+  current_waypoint = 2;
+
+
+  waypoint_path.resize(4);
+  waypoint_path[0] = 1;
+  waypoint_path[1] = 2;
+  waypoint_path[2] = 3;
+  waypoint_path[3] = 12;
 
   nh.getParam("/audibot/gps/ref_lat",ref_lat);
   nh.getParam("/audibot/gps/ref_lon",ref_lon);
